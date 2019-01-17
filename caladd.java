7591193ceaffd95058d3d7919cb2a9af46b93775
oormilaghantasala <oormilaghantasala@gmail.com>
2019-01-17 09:52:06 +0530

    Initial commit

---
+++ caladd.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+public class caladd {
+	public static void main(String args[]){
+		float a,b,res;
+		char choice, ch;
+		Scanner scan = new Scanner(System.in);
+		
+		do{
+			System.out.print("1. ADD TWO Numbers\n");
+			System.out.print("Enter Your Choice : ");
+			choice = scan.next().charAt(0);
+			switch(choice){
+				case '1' : System.out.print("Enter Two Numbers");
+						   a = scan.nextFloat();
+						   b = scan.nextFloat();
+						   res = a+b;
+						   System.out.print("Result = " + res);
+						   break;
+						   
+				case '2' : System.exit(0);
+						   break;
+						   
+				default : System.out.print("INVALID CHOICE !!!");
+				          break;
+			}
+			System.out.print("\n----------------------------------------\n");
+		}while(choice != 2);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
